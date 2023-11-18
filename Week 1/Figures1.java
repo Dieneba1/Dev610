a8b62249388ea004116889fd51e05f23d1471006
dieneba diaby <dienebadiaby@dienebas-MacBook-Pro-2.local>
2023-11-18 13:07:15 -0800

    Uploading Week 1 files

---
+++ Week 1/Figures1.java
@@ -0,0 +1,27 @@
+// unstructured code
+public class Figures1 {
+    public static void main(String[] args) {
+        System.out.println("  ______");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+        System.out.println("\\        /");
+        System.out.println(" \\______/");
+        System.out.println();
+        System.out.println("\\        /");
+        System.out.println(" \\______/");
+        System.out.println("+--------+");
+        System.out.println();
+        System.out.println("  ______");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+        System.out.println("|  STOP  |");
+        System.out.println("\\        /");
+        System.out.println(" \\______/");
+        System.out.println();
+        System.out.println("  ______");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+        System.out.println("+--------+");
+    }
+}
+
